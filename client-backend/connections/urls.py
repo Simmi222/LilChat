9aa65d3635cd9275c8ba1bfb501b4a15c8c54ffc
simaran sahu <simaransahu4@gmail.com>
2026-05-31 01:40:21 +0530

    fix urls

--- client-backend/connections/urls.py
+++ client-backend/connections/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 from .views import ConnectionViewSet
 
-router = DefaultRouter()
+router = SimpleRouter()
 router.register(r'connections', ConnectionViewSet, basename='connection')
 
 urlpatterns = [
--- client-backend/posts/urls.py
+++ client-backend/posts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 from .views import PostViewSet, CommentViewSet
 
-router = DefaultRouter()
+router = SimpleRouter()
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
 
